312779ebae3c5f909c8f93514a6fe37ba23bbaa2
Ihor Sheremet <igor.sheremet@gmail.com>
2021-04-14 21:26:43 +0300

    Create a Worker

--- app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
+++ app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
@@ -16,3 +16,19 @@
  */
 
 package com.example.android.devbyteviewer.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.android.devbyteviewer.repository.VideosRepository
+import retrofit2.HttpException
+
+class RefreshDataWork(appContext: Context, params: WorkerParameters)
+    : CoroutineWorker(appContext, params) {
+    override suspend fun doWork(): Result = try {
+        VideosRepository().refreshVideos()
+        Result.success()
+    } catch (e: HttpException) {
+        Result.failure()
+    }
+}
